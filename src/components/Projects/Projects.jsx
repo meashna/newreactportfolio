b6c43ab7ba3bc99bd39a312b52afe295bfddca41
ASHNA T <cs25ashn@kmeacollege.ac.in>
2023-03-02 19:18:22 +0530

    add new project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,3 @@
-
 import "./Projects.css";
 import img1 from "../../img/hackaway.png";
 import img2 from "../../img/hackclub.png";
@@ -9,236 +8,251 @@ import img8 from "../../img/rock.png";
 import img5 from "../../img/drumpad.png";
 import img4 from "../../img/reactcalc.png";
 import img9 from "../../img/api.png";
+import img10 from "../../img/ecommerceclone.png";
 import React, { useContext } from "react";
 import { themeContext } from "../../Context";
 import { Link } from "react-scroll";
 
-
-
 const Projects = () => {
-    // context
-    const theme = useContext(themeContext);
-    const darkMode = theme.state.darkMode;
-  
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-    <section id="projects"> 
+    <section id="projects">
       <h5>My Recent Works</h5>
-      
+
       <div className=" portfolio_container">
-
-
-
-      <article className="portfolio_items">
+        <article className="portfolio_items">
           <div className="portfolio_items-image">
-            <img src={img9} alt=""/>
+            <img src={img10} alt="" />
+          </div>
+          <h3>An ecommerce website created using react+tailwindcss+redux .</h3>
+          <div className="portfolio_items-cta">
+            <a
+              href="https://github.com/meashna/react_ecommerce.git"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="button i-button">Github</button>
+            </a>
+
+            <a
+              href="https://avecart.netlify.app/"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="button i-button">Site</button>
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_items">
+          <div className="portfolio_items-image">
+            <img src={img9} alt="" />
           </div>
           <h3>Weather API build on TypeScript React Tailwind_css . </h3>
           <div className="portfolio_items-cta">
-                <a   href="https://github.com/meashna/tsx_vite_weatherAPI"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://github.com/meashna/tsx_vite_weatherAPI"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Github</button>
-              </a>
+            </a>
 
-              <a   href="https://tsx-weatherapi.netlify.app/"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://tsx-weatherapi.netlify.app/"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Site</button>
-              </a>
-          
+            </a>
           </div>
-          
         </article>
-        
 
-        
         <article className="portfolio_items">
           <div className="portfolio_items-image">
-            <img src={img1} alt=""/>
+            <img src={img1} alt="" />
           </div>
-          <h3>A website build  for  Hackathon . </h3>
+          <h3>A website build for Hackathon . </h3>
           <div className="portfolio_items-cta">
-                <a   href="https://github.com/Hack-Club-Kmea/hackaway-website"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://github.com/Hack-Club-Kmea/hackaway-website"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Github</button>
-              </a>
+            </a>
 
-              <a   href="https://hackaway.hackclub.com/index.html"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://hackaway.hackclub.com/index.html"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Site</button>
-              </a>
-          
+            </a>
           </div>
-          
         </article>
 
-      
         <article className="portfolio_items">
           <div className="portfolio_items-image">
-            <img src={img2} alt=""/>
+            <img src={img2} alt="" />
           </div>
-          <h3>A  react website build  for a Community . </h3>
+          <h3>A react website build for a Community . </h3>
           <div className="portfolio_items-cta">
-                <a   href="https://github.com/meashna/HC-Website"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://github.com/meashna/HC-Website"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Github</button>
-              </a>
+            </a>
 
-              <a   href="https://spiffy-squirrel-c376e2.netlify.app/"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://spiffy-squirrel-c376e2.netlify.app/"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Site</button>
-              </a>
-          
+            </a>
           </div>
-          
         </article>
 
         <article className="portfolio_items">
           <div className="portfolio_items-image">
-            <img src={img3} alt=""/>
+            <img src={img3} alt="" />
           </div>
           <h3>Portfolio website of me . </h3>
           <div className="portfolio_items-cta">
-                <a   href="https://github.com/meashna/newreactportfolio"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://github.com/meashna/newreactportfolio"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Github</button>
-              </a>
+            </a>
 
-              <a 
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Site</button>
-              </a>
-
-              
-          
+            </a>
           </div>
-          
         </article>
-
 
         <article className="portfolio_items">
           <div className="portfolio_items-image">
-            <img src={img4} alt=""/>
+            <img src={img4} alt="" />
           </div>
           <h3>React Calculator made for learning . </h3>
           <div className="portfolio_items-cta">
-                <a   href="https://github.com/meashna/React-calculator"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://github.com/meashna/React-calculator"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Github</button>
-              </a>
+            </a>
 
-              <a   href="https://fanciful-bubblegum-c887b7.netlify.app/"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://fanciful-bubblegum-c887b7.netlify.app/"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Site</button>
-              </a>
-          
+            </a>
           </div>
-          
         </article>
 
-        
         <article className="portfolio_items">
           <div className="portfolio_items-image">
-            <img src={img5} alt=""/>
+            <img src={img5} alt="" />
           </div>
           <h3>Drum pad </h3>
           <div className="portfolio_items-cta">
-                <a   href="https://github.com/meashna/Drum-pad"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://github.com/meashna/Drum-pad"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Github</button>
-              </a>
+            </a>
 
-              <a   href="https://meashna.github.io/Drum-pad/"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://meashna.github.io/Drum-pad/"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Site</button>
-              </a>
-          
+            </a>
           </div>
-          
         </article>
 
         <article className="portfolio_items">
           <div className="portfolio_items-image">
-            <img src={img6} alt=""/>
+            <img src={img6} alt="" />
           </div>
           <h3>UI for a home-interior design site . </h3>
           <div className="portfolio_items-cta">
-                
-
-              <a   href="https://www.figma.com/proto/Krik5IeOOq27HILmWhSxYx/SHE.Phone%2FTablet?scaling=scale-down&page-id=0%3A1&node-id=1%3A3"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://www.figma.com/proto/Krik5IeOOq27HILmWhSxYx/SHE.Phone%2FTablet?scaling=scale-down&page-id=0%3A1&node-id=1%3A3"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Demo</button>
-              </a>
-          
+            </a>
           </div>
-          
         </article>
 
         <article className="portfolio_items">
           <div className="portfolio_items-image">
-            <img src={img7} alt=""/>
+            <img src={img7} alt="" />
           </div>
           <h3>UI for boat Nirvana website . </h3>
           <div className="portfolio_items-cta">
-
-
-              <a   href="https://www.figma.com/proto/3lo9mX2Uhg7NRXfcQ4eE0K/BOAT?scaling=scale-down&page-id=0%3A1&node-id=5%3A34"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://www.figma.com/proto/3lo9mX2Uhg7NRXfcQ4eE0K/BOAT?scaling=scale-down&page-id=0%3A1&node-id=5%3A34"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Demo</button>
-              </a>
-          
+            </a>
           </div>
-          
         </article>
 
         <article className="portfolio_items">
-        <div className="portfolio_items-image">
-            <img src={img8} alt=""/>
+          <div className="portfolio_items-image">
+            <img src={img8} alt="" />
           </div>
           <h3>Python Rock-Papper-Scissor game using python </h3>
           <div className="portfolio_items-cta">
-                <a   href="https://github.com/meashna/RockPaperScissor.py"
-                      style={{ color: "white" }}
-                      target="_blank" 
-                      rel="noopener noreferrer">
+            <a
+              href="https://github.com/meashna/RockPaperScissor.py"
+              style={{ color: "white" }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="button i-button">Github</button>
-              </a>
-
-             
-          
+            </a>
           </div>
-          
         </article>
-
-        
-        
-       
       </div>
     </section>
   );
